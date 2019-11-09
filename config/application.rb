757a2b4d35e7fe3925b6627cd3899f17b1c3a8e9
takayuki takahashi <ttkaws15@gmail.com>
2019-11-09 15:41:18 +0900

    Add:モデル作成

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,13 @@ Bundler.require(*Rails.groups)
 
 module Manyou
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.jbuilder false
+    end
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
