d25b752bf9c0e19df0e4167357735596a0c0b776
takayuki-takahashi-dic <ttkaws15@gmail.com>
2019-11-12 09:09:31 +0900

    Add：日本語化ファイル/flashの日本語化

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,9 @@ Bundler.require(*Rails.groups)
 module Manyou
   class Application < Rails::Application
     config.load_defaults 5.2
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
     config.generators do |g|
       g.assets false
       g.helper false
