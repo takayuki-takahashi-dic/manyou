e0aa31bc17ae8c8caea5d219b6824543bcfb9fb6
takayuki-takahashi-dic <ttkaws15@gmail.com>
2019-11-12 16:19:11 +0900

    Add: バリデーション/null制約

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_09_063513) do
+ActiveRecord::Schema.define(version: 2019_11_12_071141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
