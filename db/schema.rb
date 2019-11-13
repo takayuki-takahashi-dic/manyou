06972af7867d7f1c297de0659c81e6b53cbbfceb
takayuki-takahashi-dic <ttkaws15@gmail.com>
2019-11-13 14:17:09 +0900

    add: deadlineカラムを追加(null制約/デフォルト値あり)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_071141) do
+ActiveRecord::Schema.define(version: 2019_11_13_045156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_11_12_071141) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "deadline", null: false
   end
 
 end
